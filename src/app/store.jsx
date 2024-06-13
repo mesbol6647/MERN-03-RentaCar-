acb568c8ec648929eeeb7feda6d344f4a1eafe89
Mesut <mesbol6647@gmail.com>
2024-06-14 01:12:18 +0300

    store

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,7 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
-import{ persistStore, persistReducer} from "redux-persist";
-import storage from "redux-persist/lib/storage"
+import carReducer from "../features/carSlice";
+import{ 
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  } from "redux-persist";
+// import storage from "redux-persist/lib/storage" //?localstorage
+import storage from "redux-persist/lib/storage/session" //?sessionstorage
 
 
 const persistConfig = {
@@ -12,8 +23,15 @@ const persistedReducer = persistReducer(persistConfig, authReducer)
 const store = configureStore({
   reducer: {
     auth: persistedReducer,
+    car: carReducer
   },
   devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor= persistStore(store)
